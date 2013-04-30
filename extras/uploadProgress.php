eea3a500767cedb98084f78e115429d0538944b5
Seth <sethdturner23@gmail.com>
2013-04-30 00:57:54 -0400

    Fixed formatting

--- extras/uploadProgress.php
+++ extras/uploadProgress.php
@@ -16,19 +16,19 @@
 if (isset($_REQUEST['getkey'])) 
   exit(json_encode(array('key' => uniqid())));
 elseif (isset($_REQUEST['progresskey'])) 
-	$status = apc_fetch('upload_'.$_REQUEST['progresskey']);
+  $status = apc_fetch('upload_'.$_REQUEST['progresskey']);
 else 
-	exit(json_encode(array('success' => false)));
+  exit(json_encode(array('success' => false)));
 
 $pct = 0; $size = 0;
 
 if (is_array($status)) {
-	if (array_key_exists('total', $status) && array_key_exists('current', $status)) {
-		if ($status['total'] > 0) {
-		  $pct = round( ( $status['current'] / $status['total']) * 100 );
+  if (array_key_exists('total', $status) && array_key_exists('current', $status)) {
+    if ($status['total'] > 0) {
+      $pct = round( ( $status['current'] / $status['total']) * 100 );
       $size = round($status['total'] / 1024);
-		}
-	}
+    }
+  }
 }
 
 echo json_encode(array('success' => true, 'pct' => $pct, 'size' => $size));

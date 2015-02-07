05a5c2c3653f2e8f37d4b151bbb8a5ed274338ca
Seth Turner <sethdturner23@gmail.com>
2015-02-07 00:17:38 -0500

    Added basic use example

---
+++ examples/basic_example/file_upload.php
@@ -0,0 +1,18 @@
+<?php
+
+require(dirname(__FILE__) . '/../../Uploader.php');
+
+// Directory where we're storing uploaded images
+// Remember to set correct permissions or it won't work
+$upload_dir = '/temp_images/';
+
+$uploader = new FileUpload('uploadfile');
+
+// Handle the upload
+$result = $uploader->handleUpload($upload_dir);
+
+if (!$result) {
+  exit(json_encode(array('success' => false, 'msg' => $uploader->getErrorMsg())));  
+}
+
+echo json_encode(array('success' => true));

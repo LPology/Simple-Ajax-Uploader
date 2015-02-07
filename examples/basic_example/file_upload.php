1c3bb12a4ad57e543351523caced7ab208a07d42
Seth Turner <sethdturner23@gmail.com>
2015-02-07 00:19:18 -0500

    Added basic use example

--- examples/basic_example/file_upload.php
+++ examples/basic_example/file_upload.php
@@ -1,10 +1,9 @@
 <?php
-
 require(dirname(__FILE__) . '/../../Uploader.php');
 
 // Directory where we're storing uploaded images
 // Remember to set correct permissions or it won't work
-$upload_dir = '/temp_images/';
+$upload_dir = '/upload_files/';
 
 $uploader = new FileUpload('uploadfile');
 
